fa3b3b920d778ad4f9c5b3de932f4d4d9f1422d4
Sroller1 <ziemniok20@gmail.com>
2024-12-18 00:10:16 +0100

    Add files via upload

--- shopping.py
+++ shopping.py
@@ -64,7 +64,7 @@ fig, ax = plt.subplots()
 filtered_data["Shipping Type"].hist(bins=20, ax=ax)
 ax.set_xlabel("Rodzaj wysyłki")
 ax.set_ylabel("Średnia kwota zakupów (USD)")
-plt.hist("Rodzaj wysyłki", rwidth=2)
+plt.hist("Rodzaj wysyłki", rwidth=10)
 st.pyplot(fig)
 
 # Wykres 5: Zakupy wg regionu
